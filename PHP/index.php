83377553f300899f391433f6ab64cb2167526a9b
Sophia <sophiaackermann@gmx.de>
2021-06-05 11:01:59 +0200

    update

--- PHP/index.php
+++ PHP/index.php
@@ -3,9 +3,8 @@
 
 <head>
     <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Startseite Grishaverse-Datenbank</title>
-
     <link rel='stylesheet' href='../CSS/general_style.css'>
 </head>
 
@@ -14,16 +13,14 @@
     <h1>Das Grishaverse</h1>
 
     <nav>
-        <div class='nav-bar'>
-            <tr>
-                <td><a href='index.php' class='active'>Startseite</a></td>
-                <td>Das Grishaverse</td>
-                <td>Glossar</td>
-                <td>Quellen</td>
-                <td>Impressum</td>
-                <td>Login</td>
-            </tr>
-        </div>
+         <ul>
+            <li><a href='index.php' class='active'>Startseite</a></li>
+            <li>Das Grishaverse</li>
+            <li>Glossar</li>
+            <li>Quellen</li>
+            <li>Impressum</li>
+            <li>Login</li>
+        </ul>
     </nav>
 
     <?php
