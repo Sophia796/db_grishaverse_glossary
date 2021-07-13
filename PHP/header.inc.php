28d4b56c3daef6cf2868643675caefb45ce4462a
Sophia <sophiaackermann@gmx.de>
2021-07-13 12:20:21 +0200

    Make Navbar responsive

--- PHP/header.inc.php
+++ PHP/header.inc.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
-<html lang='de'>
+<html lang="de">
 
 <head>
-    <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$site_name?></title>
-    <link rel='stylesheet' href='../CSS/style.css'>
-    <link rel='stylesheet' href='../CSS/header_footer.css'>
-    <link rel='stylesheet' href='../CSS/navbar.css'>
-    <link rel='stylesheet' href='../CSS/slideshow.css'>
-    <link rel='stylesheet' href='../CSS/flexbox.css'>
-    <link rel='stylesheet' href='../CSS/glossary.css'>
-    <link rel='stylesheet' href='../CSS/form.css'>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/header_footer.css">
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <link rel="stylesheet" href="../CSS/slideshow.css">
+    <link rel="stylesheet" href="../CSS/flexbox.css">
+    <link rel="stylesheet" href="../CSS/glossary.css">
+    <link rel="stylesheet" href="../CSS/form.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- JQuery -->
-    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Select2 Bibliothek-->
-    <link href='https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css' rel='stylesheet'/>
-    <script src='https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js'></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <body>
-    <div class='header'>
+    <div class="header">
         <h1>GRISHAVERSE</h1>
     </div>
--- PHP/navbar.inc.php
+++ PHP/navbar.inc.php
@@ -1,3 +1,4 @@
+    <!--
     <nav>
          <ul>
             <li><a href="index.php"class="active">Startseite</a></li>
@@ -19,5 +20,43 @@
     </nav>
 
     <br>
+--> 
 
-    <script>src="../Javascript/navbar.js"></script>    
+    <div class="topnav" id="myTopnav">
+    	<a href="index.php" class="active">Startseite</a>
+        <div class="dropdown">
+            <a href="about.php">
+                <button class="dropbtn">Das Grishaverse
+                    <i class="fa fa-caret-down"></i>
+                </button>
+            </a>
+            <div class="dropdown-content">
+                <a href="about.php#a1">Legenden der Grisha</a>
+                <a href="about.php#a2">Glory or Grave</a>
+                <a href="about.php#a3">King of Scars</a>
+                <a href="about.php#a4">Begleitbücher</a>
+                <a href="about.php#a5">Serie: Shadow and Bone</a>
+            </div>
+        </div>
+        <a href="author.php">Leigh Bardugo</a>
+        <a href="glossary.php">Glossar</a>
+        <a href="map.php">Karte</a>
+        <a class="right" href="#">Login</a>
+        <a href="javascript:void(0);" class="icon" onclick="makeResponsive()">&#9776;</a>
+    </div>
+
+    <br>
+
+    <script>
+        /* Responsive-Design */
+        function makeResponsive() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>
+
+    <script>src="../Javascript/navbar.js"></script>   
