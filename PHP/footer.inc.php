fd7214fe0039347e2527935f49e79bf74575880f
Sophia <sophiaackermann@gmx.de>
2021-07-13 10:30:55 +0200

    Make Footer responsive

--- PHP/footer.inc.php
+++ PHP/footer.inc.php
@@ -3,7 +3,7 @@
 
     <div class="footer">
         <ul>
-            <li style="float:left"><a>© S. Ackermann 2021</a></li>
+            <li class="left"><a>© S. Ackermann 2021</a></li>
             <li><a href="impressum.php">Impressum</a></li>
             <li><a href="privacy.php">Datenschutz</a></li>
             <li><a href="sources.php">Quellen</a></li>
