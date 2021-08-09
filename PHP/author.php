6985ecb5a551e92de88c0a4cf4d721e7a7d4ff25
Sophia <sophiaackermann@gmx.de>
2021-08-09 13:24:21 +0200

    Update Text

--- PHP/author.php
+++ PHP/author.php
@@ -1,22 +1,22 @@
 <?php 
-$site_name = 'Grishaverse-Datenbank: Leigh Bardugo';
-include ('header.inc.php'); 
-include ('navbar.inc.php');
+$site_name = "Grishaverse-Datenbank: Leigh Bardugo";
+include ("header.inc.php"); 
+include ("navbar.inc.php");
 ?>
 
-    <div class='flex-container'>
-        <div class='item2'>
+    <div class="flex-container">
+        <div class="item2">
             <h2>Leigh Bardugo</h2>
-            <p style='text-align: center'>Leigh Bardugo ist eine Bestsellerautorin von Fantasy-Romanen und Schöpferin des Grishaverse.
+            <p style="text-align: center">Leigh Bardugo ist eine Bestsellerautorin von Fantasy-Romanen und Schöpferin des Grishaverse.
             Sie wurde in Jerusalem geboren, wuchs in Südkalifornien auf und machte ihren Abschluss an der 
             Yale University.
             Nach Stationen im Journalismus und im Marketing kam sie schließlich als 
             Special Effects-Designerin zum Film.<br>
             Leigh lebt und schreibt in Los Angeles.</p>
         </div>
-        <div class='item2'>
-            <img src='../images/images/leigh_bardugo.jpg' style='width:60%'>
+        <div class="item2">
+            <img src="../images/images/leigh_bardugo.jpg" style="width:60%">
         </div>
     </div>
 
-<?php include ('footer.inc.php'); ?>
+<?php include ("footer.inc.php"); ?>
